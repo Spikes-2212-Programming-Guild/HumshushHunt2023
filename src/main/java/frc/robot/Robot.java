1db0c34d7890442408d2506f5d9ac28370a703ff
Programming Guild <tmp@tmp.com>
2023-01-16 19:36:19 +0200

    created Drivetrain.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,68 +4,72 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.command.drivetrains.commands.DriveArcade;
+import com.spikes2212.util.PlaystationControllerWrapper;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
+    @Override
+    public void robotInit() {
 
-  }
+    }
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void disabledInit() {
+    @Override
+    public void disabledInit() {
 
-  }
+    }
 
-  @Override
-  public void disabledPeriodic() {
+    @Override
+    public void disabledPeriodic() {
 
-  }
+    }
 
-  @Override
-  public void autonomousInit() {
+    @Override
+    public void autonomousInit() {
 
-  }
+    }
 
-  @Override
-  public void autonomousPeriodic() {
+    @Override
+    public void autonomousPeriodic() {
 
-  }
+    }
 
-  @Override
-  public void teleopInit() {
+    @Override
+    public void teleopInit() {
+    }
 
-  }
+    @Override
+    public void teleopPeriodic() {
+    }
 
-  @Override
-  public void teleopPeriodic() {
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
 
-  }
+    @Override
+    public void testPeriodic() {
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
+    @Override
+    public void simulationInit() {
 
-  }
+    }
 
-  @Override
-  public void simulationInit() {
+    @Override
+    public void simulationPeriodic() {
 
-  }
-
-  @Override
-  public void simulationPeriodic() {
-
-  }
+    }
 }
