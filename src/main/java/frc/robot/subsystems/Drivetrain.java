e4c22444bfb0e845b50350131ccaa7abc2ad61b2
Spikes <Spikes.no2212@gmail.com>
2023-01-19 21:44:54 +0200

    changed extension to SparkMaxTankDrivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,16 +3,15 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
-import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.command.drivetrains.smartmotorcontrollerdrivetrain.SparkMaxTankDrivetrain;
 import com.spikes2212.control.PIDSettings;
 import com.spikes2212.dashboard.Namespace;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotMap;
 
 import java.util.function.Supplier;
 
-public class Drivetrain extends TankDrivetrain {
+public class Drivetrain extends SparkMaxTankDrivetrain {
 
     public static final double WHEEL_DIAMETER_IN_INCHES = -1;
     public static final double INCHES_TO_CM = -1;
@@ -20,11 +19,6 @@ public class Drivetrain extends TankDrivetrain {
     public static final double DISTANCE_PER_PULSE = WHEEL_DIAMETER_IN_INCHES * INCHES_TO_CM * GEAR_RATIO * Math.PI;
 
     private static Drivetrain instance;
-
-    private final CANSparkMax left1;
-    private final CANSparkMax left2;
-    private final CANSparkMax right1;
-    private final CANSparkMax right2;
 
     private final RelativeEncoder leftEncoder;
     private final RelativeEncoder rightEncoder;
@@ -63,20 +57,16 @@ public class Drivetrain extends TankDrivetrain {
     }
 
     private Drivetrain(CANSparkMax left1, CANSparkMax left2, CANSparkMax right1, CANSparkMax right2) {
-        super(new MotorControllerGroup(
-                        new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_1,
-                                CANSparkMaxLowLevel.MotorType.kBrushless),
-                        new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_2,
-                                CANSparkMaxLowLevel.MotorType.kBrushless)),
-                new MotorControllerGroup(
-                        new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_1,
-                                CANSparkMaxLowLevel.MotorType.kBrushless),
-                        new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_2,
-                                CANSparkMaxLowLevel.MotorType.kBrushless)));
-        this.left1 = left1;
-        this.left2 = left2;
-        this.right1 = right1;
-        this.right2 = right2;
+        super(
+                "drivetrain",
+                new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_1,
+                        CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_2,
+                        CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_1,
+                        CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_2,
+                        CANSparkMaxLowLevel.MotorType.kBrushless));
         this.leftEncoder = left1.getEncoder();
         this.rightEncoder = right1.getEncoder();
         leftEncoder.setPositionConversionFactor(DISTANCE_PER_PULSE);
@@ -90,6 +80,7 @@ public class Drivetrain extends TankDrivetrain {
         leftEncoder.setPosition(0);
         rightEncoder.setPosition(0);
     }
+
     public PIDSettings getDrivePIDSettings() {
         return drivePIDSettings;
     }
@@ -111,8 +102,6 @@ public class Drivetrain extends TankDrivetrain {
             }
         });
         namespace.putNumber("left neo 1 encoder value", this::getLeftEncoderPosition);
-        namespace.putNumber("left neo 2 encoder value", left2.getEncoder()::getPosition);
         namespace.putNumber("right neo 1 encoder value", this::getRightEncoderPosition);
-        namespace.putNumber("right neo 2 encoder value", right2.getEncoder()::getPosition);
     }
 }
