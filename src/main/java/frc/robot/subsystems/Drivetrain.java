8693abc6c1cbd426a0b0d5441be00dfd3a0230aa
AmitBrunicki <amit12bro@gmail.com>
2023-01-19 21:19:55 +0200

    Drivetrain with remarkable PID :)

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,24 +4,33 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.command.drivetrains.smartmotorcontrollerdrivetrain.SparkMaxTankDrivetrain;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.dashboard.ChildNamespace;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.RobotMap;
+
 import java.util.function.Supplier;
 
-public class Drivetrain extends TankDrivetrain {
+public class Drivetrain extends SparkMaxTankDrivetrain {
 
     private static Drivetrain instance;
 
-    private final Supplier<Double> kPDrive = namespace.addConstantDouble("kP", 0);
-    private final Supplier<Double> kIDrive = namespace.addConstantDouble("kI", 0);
-    private final Supplier<Double> kDDrive = namespace.addConstantDouble("kD", 0);
-    private final Supplier<Double> toleranceDrive = namespace.addConstantDouble("tolerance", 0);
-    private final Supplier<Double> waitTimeDrive = namespace.addConstantDouble("wait time", 0);
+    ChildNamespace drivePID = (ChildNamespace) namespace.addChild("drive pid");
+    private final Supplier<Double> kPDrive = drivePID.addConstantDouble("kP", 0);
+    private final Supplier<Double> kIDrive = drivePID.addConstantDouble("kI", 0);
+    private final Supplier<Double> kDDrive = drivePID.addConstantDouble("kD", 0);
+    private final Supplier<Double> toleranceDrive = drivePID.addConstantDouble("tolerance", 0);
+    private final Supplier<Double> waitTimeDrive = drivePID.addConstantDouble("wait time", 0);
+    private final PIDSettings drivePIDSettings = new PIDSettings(kPDrive, kIDrive, kDDrive, toleranceDrive, waitTimeDrive);
 
-    private final CANSparkMax left1;
-    private final CANSparkMax left2;
-    private final CANSparkMax right1;
-    private final CANSparkMax right2;
+    private final ChildNamespace anglePID = (ChildNamespace) namespace.addChild("angle pid");
+    private final Supplier<Double> kPAngle = anglePID.addConstantDouble("kP", 0);
+    private final Supplier<Double> kIAngle = anglePID.addConstantDouble("kI", 0);
+    private final Supplier<Double> kDAngle = anglePID.addConstantDouble("kD", 0);
+    private final Supplier<Double> waitTimeAngle = anglePID.addConstantDouble("wait time", 0);
+    private final Supplier<Double> toleranceAngle = anglePID.addConstantDouble("tolerance", 0);
+    private final PIDSettings anglePIDSettings = new PIDSettings(kPAngle, kIAngle, kDAngle, waitTimeAngle,toleranceAngle);
 
     private RelativeEncoder leftEncoder;
     private RelativeEncoder rightEncoder;
@@ -38,14 +47,18 @@ public class Drivetrain extends TankDrivetrain {
     }
 
     public Drivetrain(CANSparkMax left1, CANSparkMax left2, CANSparkMax right1, CANSparkMax right2) {
-        super("drivetrain", new MotorControllerGroup(left1, left2), new MotorControllerGroup(right1, right2));
-        this.left1 = left1;
-        this.left2 = left2;
-        this.right1 = right1;
-        this.right2 = right2;
+        super("drivetrain", left1, left2, right1, right2);
         this.leftEncoder = left1.getEncoder();
         this.rightEncoder = right1.getEncoder();
         configureDashboard();
+    }
+
+    public PIDSettings getDrivePIDSettings() {
+        return drivePIDSettings;
+    }
+
+    public PIDSettings getAnglePIDSettings() {
+        return anglePIDSettings;
     }
 
     @Override
