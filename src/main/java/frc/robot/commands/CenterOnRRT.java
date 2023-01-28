9fbd692572c2cb4cac834676be50265c2edbdf0a
Spikes <Spikes.no2212@gmail.com>
2023-01-28 15:52:56 +0200

    created CenterOnRRT

--- src/main/java/frc/robot/commands/CenterOnRRT.java
+++ src/main/java/frc/robot/commands/CenterOnRRT.java
@@ -4,19 +4,19 @@ import com.spikes2212.command.drivetrains.commands.DriveArcadeWithPID;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
 
-public class CenterOnGamePiece extends DriveArcadeWithPID {
+public class CenterOnRRT extends DriveArcadeWithPID {
 
     private final Vision vision;
     private final int pipeline;
 
-    public CenterOnGamePiece(Drivetrain drivetrain, Vision vision, int pipeline) {
-        super(drivetrain, vision::getPhotonVisionYaw, 0, 0, drivetrain.getAnglePIDSettings());
+    public CenterOnRRT(Drivetrain drivetrain, Vision vision, int pipeline) {
+        super(drivetrain, () -> vision.getLimelightYaw(), 0, 0, drivetrain.getAnglePIDSettings());
         this.vision = vision;
         this.pipeline = pipeline;
     }
 
     @Override
     public void initialize() {
-        vision.changePhotonVisionPipeline(pipeline);
+        vision.changeLimelightPipeline(pipeline);
     }
 }
