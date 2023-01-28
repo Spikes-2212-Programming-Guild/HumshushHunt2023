9a4ab4238b94ffaa928beb4e85ad3c23319eaa52
Spikes <Spikes.no2212@gmail.com>
2023-01-28 16:03:32 +0200

    created OpenGripper and CloseGripper

---
+++ src/main/java/frc/robot/commands/CloseGripper.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Gripper;
+
+public class CloseGripper extends InstantCommand {
+
+    public CloseGripper(Gripper gripper) {
+        addRequirements(gripper);
+        gripper.closeGripper();
+    }
+}
---
+++ src/main/java/frc/robot/commands/OpenGripper.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Gripper;
+
+public class OpenGripper extends InstantCommand {
+
+    public OpenGripper(Gripper gripper) {
+        addRequirements(gripper);
+        gripper.openGripper();
+    }
+}
