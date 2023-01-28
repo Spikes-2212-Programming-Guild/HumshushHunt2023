505a435c1b0e89d515254a81cdc68e98a2654c35
Spikes <Spikes.no2212@gmail.com>
2023-01-28 11:57:00 +0200

    saving progress

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,30 +2,34 @@ package frc.robot.subsystems;
 
 import com.spikes2212.command.DashboardedSubsystem;
 import com.spikes2212.util.Limelight;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.math.geometry.Pose3d;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 public class Vision extends DashboardedSubsystem {
+//
+//    private final CameraServer cameraServer;
+//    private final UsbCamera usbCamera;
+//    private final CvSink cvSink;
+//    private final CvSource cvSource;
 
     public static final String PHOTON_VISION_CAMERA_NAME = "photoncamera";
 
     public static final int PIPELINE_CONE_INDEX = 0;
     public static final int PIPELINE_CUBE_INDEX = 1;
 
-    public static final int PIPELINE_RETROREFLECTIVE_INDEX = 0;
-    public static final int PIPELINE_APRILTAG_INDEX = 1;
+    public static final int PIPELINE_HIGH_RRT_INDEX = 0;
+    public static final int PIPELINE_LOW_RRT_INDEX = 1;
+    public static final int PIPELINE_APRILTAG_INDEX = 2;
 
     private static Vision instance;
 
     private final PhotonCamera photonCamera;
     private final Limelight limelight;
-
-    private Vision(String namespaceName, PhotonCamera photonCamera, Limelight limelight) {
-        super(namespaceName);
-        this.photonCamera = photonCamera;
-        this.limelight = limelight;
-    }
 
     public static Vision getInstance() {
         if (instance == null) {
@@ -34,7 +38,15 @@ public class Vision extends DashboardedSubsystem {
         return instance;
     }
 
-    private double getPhotonVisionYaw() {
+    private Vision(String namespaceName, PhotonCamera photonCamera, Limelight limelight) {
+        super(namespaceName);
+        this.photonCamera = photonCamera;
+        this.limelight = limelight;
+//        this.cameraServer =
+        configureDashboard();
+    }
+
+    public double getPhotonVisionYaw() {
         PhotonPipelineResult result = photonCamera.getLatestResult();
         if (result.hasTargets()) {
             return result.getBestTarget().getYaw();
@@ -44,6 +56,10 @@ public class Vision extends DashboardedSubsystem {
 
     public Pose3d getRobotPose() {
         return limelight.getRobotPose();
+    }
+
+    public long getAprilTagID() {
+        return limelight.getID();
     }
 
     public void changePhotonVisionMode(boolean mode) {
