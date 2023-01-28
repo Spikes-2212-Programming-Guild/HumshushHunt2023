1a1d7a5adf8bee4126d5df35d28883e567272546
Spikes <Spikes.no2212@gmail.com>
2023-01-28 15:38:43 +0200

    created CenterOnGamePiece

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.control.PIDSettings;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+public class Drivetrain extends TankDrivetrain {
+
+
+    public Drivetrain(String namespaceName, MotorController left, MotorController right) {
+        super(namespaceName, left, right);
+    }
+
+    public PIDSettings getAnglePIDSettings() {
+        return null;
+    }
+}
