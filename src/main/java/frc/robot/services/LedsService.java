60226796a8043633257c1a3731b2afd20f5736b6
Programming Guild <tmp@tmp.com>
2023-02-18 12:10:21 +0200

    added gripper to LedsService

--- src/main/java/frc/robot/services/LedsService.java
+++ src/main/java/frc/robot/services/LedsService.java
@@ -3,22 +3,25 @@ package frc.robot.services;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import frc.robot.RobotMap;
+import frc.robot.subsystems.Gripper;
 
 public class LedsService {
 
-    private static final int NUMBER_OF_LEDS = 60;
+    private final Gripper gripper;
 
-    private LedsService(AddressableLED led, AddressableLEDBuffer ledBuffer, VisionService vision) {
-        this.led = led;
-        this.ledBuffer = ledBuffer;
-        this.vision = vision;
-        led.setLength(ledBuffer.getLength());
-        led.start();
-    }
+    private static final int NUMBER_OF_LEDS = 60;
 
     private static LedsService instance;
 
     private final VisionService vision;
+    private LedsService(AddressableLED led, AddressableLEDBuffer ledBuffer, VisionService vision, Gripper gripper) {
+        this.led = led;
+        this.ledBuffer = ledBuffer;
+        this.vision = vision;
+        this.gripper = gripper;
+        led.setLength(ledBuffer.getLength());
+        led.start();
+    }
 
     private final AddressableLED led;
     private final AddressableLEDBuffer ledBuffer;
@@ -26,30 +29,14 @@ public class LedsService {
     public static LedsService getInstance() {
         if (instance == null) {
             instance = new LedsService(new AddressableLED(RobotMap.PWM.LED_PORT),
-                    new AddressableLEDBuffer(NUMBER_OF_LEDS), VisionService.getInstance());
+                    new AddressableLEDBuffer(NUMBER_OF_LEDS), VisionService.getInstance(), Gripper.getInstance());
         }
         return instance;
     }
 
-    public enum Mode {
-
-        EMPTY_GRIPPER(254, 0, 0), ALLIGNED(204, 0, 254), HAS_GAME_PIECE(0, 0, 254),
-        HAS_GAME_PIECE_AND_ALLIGNED(0, 254, 0);
-
-        public final int red;
-        public final int green;
-        public final int blue;
-
-        Mode(int red, int green, int blue) {
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
-        }
-    }
-
     public void periodic() {
         boolean hasTarget = vision.limelightHasTarget();
-        boolean holdingGamePiece = vision.sensorHasTarget();
+        boolean holdingGamePiece = gripper.sensorHasTarget();
 
         if (!hasTarget && !holdingGamePiece) {
             for (int i = 0; i < ledBuffer.getLength(); i++) {
@@ -74,6 +61,22 @@ public class LedsService {
                 ledBuffer.setRGB(i, Mode.HAS_GAME_PIECE_AND_ALLIGNED.red, Mode.HAS_GAME_PIECE_AND_ALLIGNED.green,
                         Mode.HAS_GAME_PIECE_AND_ALLIGNED.blue);
             }
+        }
+    }
+
+    public enum Mode {
+
+        EMPTY_GRIPPER(254, 0, 0), ALLIGNED(204, 0, 254), HAS_GAME_PIECE(0, 0, 254),
+        HAS_GAME_PIECE_AND_ALLIGNED(0, 254, 0);
+
+        public final int red;
+        public final int green;
+        public final int blue;
+
+        Mode(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
         }
     }
 
