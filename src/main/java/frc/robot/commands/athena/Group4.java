b1ffcd7a0178758279fb642488fd384fd70036c5
The Spikes #2212 <spikes.no2212@gmail.com>
2023-05-17 11:32:38 +0300

    3

--- src/main/java/frc/robot/commands/athena/Group4.java
+++ src/main/java/frc/robot/commands/athena/Group4.java
@@ -5,8 +5,12 @@ public class Group4 extends ProgramBase {
     @Override
     public void writeProgram() {
         moveForwardInSeconds(3);
-        moveBackwardInSeconds(4);
+        moveBackwardInSeconds(3);
+        moveForwardInSeconds(3);
+        moveBackwardInSeconds(3);
+        moveForwardInSeconds(3);
           moveArm();
           releaseCube();
+        moveBackwardInSeconds(7);
         }
 }
