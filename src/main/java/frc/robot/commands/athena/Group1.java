528f2ca9378c415d124f0a9530ba61518dc80d1f
tzintzenet <spikes.no2212@gmail.com>
2023-05-17 11:25:08 +0300

    g

--- src/main/java/frc/robot/commands/athena/Group1.java
+++ src/main/java/frc/robot/commands/athena/Group1.java
@@ -4,9 +4,8 @@ public class Group1 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(2.7);
+        moveForwardInSeconds(3);
         moveArm();
         releaseCube();
-
     }
 }
