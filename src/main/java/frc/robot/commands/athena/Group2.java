0371e641d0d6485af39252ab5e739838678bbe95
Spikes  #2212 <spikes.no2212@gmail.com>
2023-05-17 10:50:57 +0300

    g

--- src/main/java/frc/robot/commands/athena/Group2.java
+++ src/main/java/frc/robot/commands/athena/Group2.java
@@ -4,6 +4,10 @@ public class Group2 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-
+    moveForwardInSeconds(1);
+    moveArm();
+    moveForwardInSeconds(0.5);
+    releaseCube();
+    moveBackwardInSeconds(1.5);
     }
 }
