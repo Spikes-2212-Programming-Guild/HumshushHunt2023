4457ab4917772d69a203c90024b7515c2ae9d11b
Nitzan <spikes.no2212@gmail.com>
2023-05-17 12:50:53 +0300

    grup3

--- src/main/java/frc/robot/commands/athena/Group3.java
+++ src/main/java/frc/robot/commands/athena/Group3.java
@@ -4,12 +4,8 @@ public class Group3 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-
-        moveForwardInSeconds(3);
-        moveBackwardInSeconds(3);
         moveForwardInSeconds(3);
         moveArm();
         releaseCube();
-        moveBackwardInSeconds(3);
     }
 }
